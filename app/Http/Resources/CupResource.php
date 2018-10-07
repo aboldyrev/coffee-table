04145095cf87d033e29e4cdf1d22963186a71e64
aboldyrev <andreybold@bk.ru>
2018-10-07 14:20:46 +0300

    Мелкие правки комментариев

--- app/Http/Resources/CupResource.php
+++ app/Http/Resources/CupResource.php
@@ -6,13 +6,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CupResource extends JsonResource
 {
-	/**
-	 * Transform the resource into an array.
-	 *
-	 * @param  \Illuminate\Http\Request $request
-	 *
-	 * @return array
-	 */
 	public function toArray($request) {
 		return parent::toArray($request);
 	}
