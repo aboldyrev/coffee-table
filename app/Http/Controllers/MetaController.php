c241495511455760aa2c6ebd4a3e5503b5216a16
aboldyrev <andreybold@bk.ru>
2018-10-07 14:15:42 +0300

    ПОправил контроллер мета-данных

--- app/Http/Controllers/MetaController.php
+++ app/Http/Controllers/MetaController.php
@@ -9,16 +9,16 @@ class MetaController extends Controller
 		return [
 			'meta' => [
 				[
-					'name' => 'Таблица',
-					'url'  => route('api.table'),
-				],
-				[
-					'name' => 'Напитки',
-					'url'  => route('api.coffees.index'),
-				],
-				[
 					'name' => 'Ёмкости',
 					'url'  => route('api.cups.index'),
+				],
+				[
+					'name' => 'Ингредиенты',
+					'url'  => route('api.ingredients.index'),
+				],
+				[
+					'name' => 'Иконки',
+					'url'  => route('api.icons.index'),
 				],
 			],
 		];
